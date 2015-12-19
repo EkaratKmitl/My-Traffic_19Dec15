b269dc4c13f99754bf97445695ce08f433967871
Ekarat <ekarat.income@gmal.com>
2015-12-19 14:46:22 +0700

    Sound Finish

--- app/src/main/java/sgs/income/ekarat/mytraffic/MainActivity.java
+++ app/src/main/java/sgs/income/ekarat/mytraffic/MainActivity.java
@@ -1,13 +1,50 @@
 package sgs.income.ekarat.mytraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit = คือการประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bind Widget
+        bindWidget();
+
+        //Button Controller
+        buttonController();
+
+    }   // Main Method
+
+    private void buttonController() {
+       aboutMeButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+               //Sound  Effect
+               MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.effect_btn_long);
+               buttonMediaPlayer.start();
+
+           } //Event
+       });
+
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+    }
+
+}  //Main Class
