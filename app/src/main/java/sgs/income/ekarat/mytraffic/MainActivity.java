5d0b93b1b70da34940c53ccef3ad867b33b50b2a
Ekarat <ekarat.income@gmal.com>
2015-12-19 11:42:38 +0700

    stat

---
+++ app/src/main/java/sgs/income/ekarat/mytraffic/MainActivity.java
@@ -0,0 +1,13 @@
+package sgs.income.ekarat.mytraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
